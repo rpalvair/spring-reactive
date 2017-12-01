6ee48bf14b3875f04272d8c008c1a110d722e0b4
ruddyp <r.palvair@alptis.fr>
2017-12-01 13:59:13 +0100

    Initial commit

---
+++ src/main/java/com/palvair/spring/reactive/infrastructure/spring/Application.java
@@ -0,0 +1,33 @@
+package com.palvair.spring.reactive.infrastructure.spring;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication(scanBasePackages = "com.palvair.spring-reactive")
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    /*@Bean
+    public Swagger2Feature swaggerFeature() {
+        return new Swagger2Feature();
+    }*/
+
+    /*@Bean
+    public JacksonJsonProvider jsonProvider(ObjectMapper objectMapper) {
+        JacksonJaxbJsonProvider provider = new JacksonJaxbJsonProvider();
+        provider.setMapper(objectMapper);
+        return provider;
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }*/
+}
